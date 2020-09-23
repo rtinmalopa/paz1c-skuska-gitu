40550c12f81f6f5e752aa7471ad6feb714d77045
student <student@158.197.35.232>
2020-09-23 12:01:13 +0200

    moja zmena

--- src/Prva.java
+++ src/Prva.java
@@ -5,6 +5,7 @@ public class Prva {
 		System.out.println("Martin Palo");
 		System.out.println("babkina fujara");
 		System.out.println("Martin sus :eyes:");
+		System.out.println("Musim pisat do prvej triedy lebo aj napriek tomu ze si dostal vyplatu tak si nerobis svoju pracu na 100% :peposad:");
 	}
 
 }

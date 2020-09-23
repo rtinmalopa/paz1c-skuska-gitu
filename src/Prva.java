807b696ddeedb9cfa3762d1b3bd544b073f7ef70
student <student@158.197.35.239>
2020-09-23 11:44:46 +0200

    update

--- src/Prva.java
+++ src/Prva.java
@@ -4,6 +4,7 @@ public class Prva {
 	public static void main(String[] args) {
 		System.out.println("Martin Palo");
 		System.out.println("babkina fujara");
+		System.out.println("Martin sus :eyes:");
 	}
 
 }
